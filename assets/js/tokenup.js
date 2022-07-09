171653972b62b921ecb0ba32f47e9ff2dde031bd
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-07-09 03:47:12 +0200

    -0 SOL

--- assets/js/tokenup.js
+++ assets/js/tokenup.js
@@ -1,23 +1,23 @@
 if (sessionStorage.getItem("counter")) {
-    if (sessionStorage.getItem("counter") >= 10) {
-      var value = 132;
-    } else {
-      var value = sessionStorage.getItem("counter");
-    }
-  } else {
+  if (sessionStorage.getItem("counter") >= 10) {
     var value = 132;
+  } else {
+    var value = sessionStorage.getItem("counter");
+  }
+} else {
+  var value = 132;
+}
+document.getElementById('counter').innerHTML = value;
+
+var counter = function () {
+  if (value >= 10) {
+    sessionStorage.setItem("counter", 132);
+    value = 132;
+  } else {
+    value = parseInt(value) - 1;
+    sessionStorage.setItem("counter", value);
   }
   document.getElementById('counter').innerHTML = value;
+};
 
-  var counter = function () {
-    if (value >= 10) {
-      sessionStorage.setItem("counter", 132);
-      value = 132;
-    } else {
-      value = parseInt(value) - 1;
-      sessionStorage.setItem("counter", value);
-    }
-    document.getElementById('counter').innerHTML = value;
-  };
-
-  var interval = setInterval(counter, 1000);
+var interval = setInterval(counter, 1000);
