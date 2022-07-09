667bfb0c5748e6c66b9c7595a24541ac728a53bd
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-07-09 05:22:38 +0200

    Update tokenup.js

--- assets/js/tokenup.js
+++ assets/js/tokenup.js
@@ -1,23 +1,31 @@
-if (sessionStorage.getItem("counter")) {
-  if (sessionStorage.getItem("counter") >= 10) {
-    var value = 34;
-  } else {
-    var value = sessionStorage.getItem("counter");
-  }
-} else {
-  var value = 34;
-}
-document.getElementById('counter').innerHTML = value;
+//fill in below as you need
+let startCount = 132;
 
-var counter = function () {
-  if (value >= 10) {
-    sessionStorage.setItem("counter", 34);
-    value = 34;
-  } else {
-    value = parseInt(value) - 1;
-    sessionStorage.setItem("counter", value);
-  }
-  document.getElementById('counter').innerHTML = value;
-};
+const	stopCount = 0,	
+		duration = 100000,//milliseconds
+		countDownElement = document.getElementById('counter'),//your element to display the countDown
+		//end fill in ...
+		
+		
+		
+		intervalTime = duration/Math.abs(startCount - stopCount);
+		
+		
+let countDown = setInterval(
 
-var interval = setInterval(counter, 1000);
+function(){
+
+if(startCount === stopCount)clearInterval(countDown)
+
+countDownElement.innerHTML = startCount;
+
+if(startCount > stopCount)	{
+startCount--
+}else{
+startCount++
+}	
+
+
+},
+intervalTime
+);
