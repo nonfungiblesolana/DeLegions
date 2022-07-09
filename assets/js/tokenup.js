c19448a7e6fc6bb439d95f0e03c51b50df2b357c
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-07-09 03:57:30 +0200

    Update tokenup.js

--- assets/js/tokenup.js
+++ assets/js/tokenup.js
@@ -1,18 +1,18 @@
 if (sessionStorage.getItem("counter")) {
   if (sessionStorage.getItem("counter") >= 10) {
-    var value = 132;
+    var value = 34;
   } else {
     var value = sessionStorage.getItem("counter");
   }
 } else {
-  var value = 132;
+  var value = 34;
 }
 document.getElementById('counter').innerHTML = value;
 
 var counter = function () {
   if (value >= 10) {
-    sessionStorage.setItem("counter", 132);
-    value = 132;
+    sessionStorage.setItem("counter", 34);
+    value = 34;
   } else {
     value = parseInt(value) - 1;
     sessionStorage.setItem("counter", value);
